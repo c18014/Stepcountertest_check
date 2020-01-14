0635f8f393b17deb8f6957885a6ff4dc3e7567a0
c18014 <hj.h.shirai@gmail.com>
2020-01-14 10:48:44 +0900

    Initial commit

---
+++ app/src/main/java/com/example/stepcountertest2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.stepcountertest2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
